da47644bfaa728b616d8396bc01a802ec565a700
Akmal <106945725+akmalkhudoynazarov@users.noreply.github.com>
2023-03-10 11:56:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Prevezto.sk</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <header>
+    <nav class="navbar">
+      <a href="" class="nav-branding">Logo</a>
+      <ul class="nav-menu">
+        <li class="nav-item"><a href="" class="nav-link">O nás</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Služby</a></li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Typy Prepravy</a>
+        </li>
+        <li class="nav-item"><a href="" class="nav-link">Rychly Dopyt</a></li>
+        <li class="nav-item"><a href="" class="nav-link">VOP</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Kontakt</a></li>
+      </ul>
+      <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <!-- Banner -->
+    <div class="banner">
+      <h1>
+        Váš Slovenský partner pre Slovenskú a <br />
+        Medzinárodnú prepravu
+      </h1>
+
+      <a href="" class="hero-btn">Rýchly Dopyt</a>
+    </div>
+
+    <div class="invisible-height"></div>
+
+    <!-- About us -->
+    <div class="about-us">
+      <h2>O NÁS</h2>
+      <hr>
+      <p>
+        Spoločnosť PrevezTo je dopravná a špedičná spoločnosť, ktorá bola
+        založená v roku 2017 v Bratislave a poskytuje prepravné i špedičné
+        služby voblasti vnútroštátnej a medzinárodnej prepravy. Dokážeme
+        zabezpečiť širokú škálu služieb v oblasti prepravy a špedície (cestná,
+        železničná, námorná, letecká, kombinovaná).</p><br>
+      <p>Vďakanašej flexibilite a partnerom patria medzi našich zákazníkov
+        nie len väčšie spoločnosti, menšie a stredné firmy ale aj súkromné
+        osoby. Dokážeme Vám pomôcť s menšími zásielkami alebo náročnějšími
+        prepravami.</p>
+      </p>
+      <div class="about-container">
+        <div>
+          <img src="img/smalltruck.jpg" alt="" />
+          <br />
+          <p>VOZIDLÁ DO 3,5t</p>
+        </div>
+        <div>
+          <img src="img/ftl.jpg" alt="" />
+          <br />
+          <p>CELOVOZOVÉ ZÁSIELKY(FTL)</p>
+        </div>
+        <div>
+          <img src="img/pallet.jpg" alt="" />
+          <br />
+          <p>ČIASTKOVÉ/DOKLÁDKY (LTL)</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Sluzby -->
+    <div class="about-us services">
+      <h2>SLUŽBY</h2>
+      <hr>
+      <p>Medzi naše hlavné devízy patrí vnútroštátna a medzinárodná preprava
+        tovaru do 3,5t za čo najvýhodnejšie ceny. Nad 3,5t poskytujeme
+        celovozové prepravyoznačované ako FTL( full to load), alebo
+        čiastkové / dokládky ako LTL( Less to load), ktoré realizujeme našimi
+        vozidlami, alebo našim oddelením špedície v Bratislave.</p> <br>
+      <p>Prečo platit za celé vozidlo keď potrebujete previezť len niekoľko paliet? A prečo
+        naopak voziť veľký náklad v niekoľkých autách? V PrevezTo máme
+        niekoľko typov vozidiel, zktorých vyberieme to najvhodnejšie pre Váš
+        náklad.</p><br>
+
+      <p>
+        Našou prioritou je zabezpečiť požadovanú prepravu tovaru, kamkoľvek v
+        Európe. Nepretržitým monitorovaním vozidiel a komunikácie s vodičmi sa
+        snažime eliminovať akékoľvek zlyhanie v procese dodania tovaru.
+      </p>
+      <div class="about-container">
+        <div>
+          <img src="img/smalltruck.jpg" alt="" />
+          <br />
+          <p>DOKUMENT</p>
+        </div>
+        <div>
+          <img src="img/ftl.jpg" alt="" />
+          <br />
+          <p>BALÍK</p>
+        </div>
+        <div>
+          <img src="img/pallet.jpg" alt="" />
+          <br />
+          <p>PALETA</p>
+        </div>
+      </div>
+
+      <p class="btn-container""><a href="" class=" hero-btn-2">Rýchly Dopyt</a></p>
+
+    </div>
+
+    <!-- TYPY PREPRAVY -->
+    <div class="about-us">
+      <h2>TYPY PREPRAVY</h2>
+      <hr>
+      <p>Zameriavame sa na prepravné množstvo od 1 palety vo vozidle do 3,5t až po 34 paliet vjednom alebo viacerých kamiónech</p>
+      <br>
+      <br>
+      <!-- delivery in 24 hours -->
+      <h3>EXPRES PREPRAVY do 24 hodín ( SAME DAY)</h3>
+      <div class="about-container">
+        <div>
+          <h4>Preprava paliet BRATISLAVA</h4>
+          <i>Expres prepravu paliet vrámci BRATISLAVY zabezpečíme behom niekoľkých minút presne podľa Vašej objednávky už od 25 € bez DPH</i>
+        </div>
+        <div>
+          <h4>Preprava paliet SLOVENSKO</h4>
+          <i>Expres prepravu paliet vrámci Slovenska zabezpečíme do 24 HODÍN vďake službe SAME DAY Vašu objednávku vyzdvihneme na dohodnutom mieste adoručíme na cieľovú adresu vjeden deň.</i>
+        </div>
+        <div>
+          <h4>Preprava paliet ZAHRANIČIE</h4>
+          <i>Expres prepravu paliet do ZAHRANIČIA zabezpečíme do 24 HODÍN vďake službe SAME DAY Vašu objednávku vyzdvihneme na dohodnutom mieste adoručíme na cieľovú adresu vjeden deň. platí pre CZ, AT, HU, PL, DE</i>
+        </div>
+      </div>
+      <p class="btn-container""><a href="" class=" hero-btn-2">Rýchly Dopyt</a></p>
+    </div>
+
+
+    <!-- delivery of packages -->
+    <div class="about-us balik">
+      <h3>PREPRAVA BALIKOV / DOKUMENTOV</h3>
+      <div class="about-container balik-container">
+        <div>
+          <h4>Expres preprava balíkov/dokumentov</h4>
+          <i>BRATISLAVA, SLOVENSKO, ČESKÁ REPUBLIKA dodanie do 24 hodín</i>
+        </div>
+        <div>
+          <h4>Expres preprava balíkov/dokumentov</h4>
+          <i>AT, DE, HU, PL Dodanie do 24 hodín</i>
+        </div>
+        <div>
+          <h4>Štandardná preprava balíkov/dokumentov</h4>
+          <i>AT, DE, HU, PL Dodanie do 48 hodín</i>
+        </div>
+        <div>
+          <h4>Preprava balíkov</h4>
+          <i>SVET</i>
+        </div>
+      </div>
+      <p class="btn-container""><a href="" class=" hero-btn-2">Rýchly Dopyt</a></p>
+    </div>
+
+    <!-- Celovozové prepravy do 3,5t -->
+    <div class="about-us">
+      <div class="about-container ltl">
+        <div>
+          <h4>Celovozové prepravy do 3,5t</h4>
+          <ul>
+            <li>Vhodné pre zásielky zaberajúce celú dodávku</li>
+            <li>Skriňová 5 paliet</li>
+            <li>Plachtová 8-10 paliet</li>
+            <li>Najmenej časovo náročný typ prepravy</li>
+            <li>Vhodný ak máte požadavku, aby sa nemanipulovalo s tovarom</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Celovozidlové prepravy (FTL)</h4>
+          <ul>
+            <li>Vhodné pre zásielky zaberajúce celé vozidlo.</li>
+            <li>Ak máte požiadavku, aby sa nemanipulovalo s tovarom.</li>
+            <li>Najmenej časovo náročný, Váš tovar ide priamo z miesta vyzdvihnutia na miesto doručenia.</li>
+            <li>Vyberieme na Vašu prepravu vozidlo presne podľa vašich požiadaviek.</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Dokládkový servis (LTL)</h4>
+          <ul>
+            <li>Vhodný pre menšie zásielky nezaberajúce celé vozidlo.</li>
+            <li>Klient nemusí platiť za celé vozidlo, len za alikvotnú časť.</li>
+            <li>Vhodné na prepravné množstvo od 1 palety po nadrozmerné zásielky.</li>
+            <li>Počas prepravy je zabezpečená profesionálna manipulácia s tovarom.</li>
+          </ul>
+        </div>
+      </div>
+      <p class="btn-container""><a href="" class=" hero-btn-2">Rýchly Dopyt</a></p>
+    </div>
+
+    <!-- Celovozové prepravy do 3,5t -->
+    <div class="about-us balik">
+      <div class="about-container balik-container">
+        <div>
+          <h4>Zásobovanie</h4>
+          <ul>
+            <li>Vhodné pre zásielky od jedného klienta smerujúce na viacero adries</li>
+            <li>Balíkové distribúcie</li>
+            <li>Paletové distribúcie</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Distribúcie</h4>
+          <ul>
+            <li>Vhodné pre zásielky zaberajúce celé vozidlo.</li>
+            <li>Vhodné pre zásielky od jedného klienta smerujúce na viacero adries</li>
+            <li>Balíkové distribúcie</li>
+            <li>Paletové distribúcie</li>
+          </ul>
+        </div>
+        <div>
+          <h4>Skladovanie</h4>
+          <ul>
+            <li>Krátkodobé a dlhodobé skladovanie</li>
+            <li>Sklad v Bratislave s dobrým napojením na diaľnicu</li>
+          </ul>
+        </div>
+      </div>
+      <p class="btn-container""><a href="" class=" hero-btn-2">Rýchly Dopyt</a></p>
+    </div>
+
+    <div class="about-us booking">
+      <h2>DOPYT/OBJEDNÁVKA</h2>
+      <div class="about-container booking-container">
+        <div>
+          <form action="contactget.php" method="post">
+            <input type="text" id="name" placeholder="Meno a Priezvisko" required><br>
+            <input type="tel" id="tel" placeholder="Mobil" required><br>
+            <input type="email" id="email" placeholder="Email" required><br>
+            <input type="text" id="wherefrom" placeholder="Odkial"><br>
+            <input type="text" id="whereto" placeholder="Kam"><br>
+            <textarea id="message" style="resize:vertical;" rows="5" placeholder="Detail prepravy"></textarea><br><br>
+            <button class="hero-btn-2" type="submit">Odoslať</button>
+          </form>
+          <?php
+          session_start(); // Start the session
+
+          if (isset($_SESSION['response'])) {
+            echo '<div class="response">' . $_SESSION['response'] . '</div>';
+            unset($_SESSION['response']); // Unset the response variable
+          }
+          ?>
+        </div>
+        <div>
+          <h4>OBJEDNÁVKY</h4>
+          <i><a href="">0948 457 746</a></i><br><br>
+          <i><a href="">objednavky@prevezto.sk</a></i>
+          <br><br>
+          <hr>
+          <h4>FAKTURÁCIA</h4>
+          <i><a href="">faktury@prevezto.sk</a></i>
+          <br><br>
+          <hr>
+          <h4>INFORMÁCIE/SPOLUPRÁCA/KARIÉRA</h4>
+          <i><a href="">ceo@prevezto.sk</a></i>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <footer>
+        <div><img src="img/LOGOc.2.png" alt=""></div>
+        <div>
+          <p>S-H Group s.r.o. <br>
+            Hronská 20 <br>
+            821 06 Bratislava <br>
+            Slovensko
+          </p>
+        </div>
+        <div>
+          <p>IČO: 50787900 <br>
+            DIČ: 2120477557 <br>
+            IČ DPH: SK2120477557
+          </p>
+        </div>
+      </footer>
+      <br>
+      <hr>
+      <p class="copyright"> &copy; Copyright <span id="currentYear"></span> | Web Powered By DigitalWeb.sk</p>
+    </div>
+
+  </main>
+  <script src="script.js"></script>
+  <script>
+    let year = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = year;
+  </script>
+</body>
+
+</html>
