b7c8ce1dba3cbbc598c6f8113822c0e55927c588
Akmal <106945725+akmalkhudoynazarov@users.noreply.github.com>
2023-03-10 14:11:54 +0100

    changed

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+// Get the form data
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$wherefrom = $_POST['wherefrom'];
+$whereto = $_POST['whereto'];
+$message = $_POST['message'];
+
+// Validate the name and email
+if (empty($name) || empty($email)) {
+  // Send an error response
+  $response = ['message' => 'Please provide your name and email.'];
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  // Send an error response
+  $response = ['message' => 'Please provide a valid email address.'];
+} else {
+  // Send the input data to the mail address
+  $to = 'objednavky@prevezto.sk';
+  $subject = 'New Form Submission';
+  $body = "Name: $name\nTel: $tel\nEmail: $email\nWherefrom: $wherefrom\nWhereto: $whereto\nMessage: $message";
+  $headers = "From: $name <$email>";
+
+  if (mail($to, $subject, $body, $headers)) {
+    // Send a success response
+    $response = ['message' => 'Your form has been submitted successfully. We will get back to you soon.'];
+  } else {
+    // Send an error response
+    $response = ['message' => 'An error occurred while sending the form data. Please try again later.'];
+  }
+}
+
+// Set the response header
+header('Content-Type: application/json');
+
+// Send the response data
+echo json_encode($response);
--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+// Select the form and add a submit event listener
+document.querySelector("form").addEventListener("submit", function (event) {
+  // Prevent the default form submission
+  event.preventDefault();
+
+  // Get the form data
+  const name = document.querySelector("#name").value;
+  const tel = document.querySelector("#tel").value;
+  const email = document.querySelector("#email").value;
+  const wherefrom = document.querySelector("#wherefrom").value;
+  const whereto = document.querySelector("#whereto").value;
+  const message = document.querySelector("#message").value;
+
+  // Create a new XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
+
+  // Set the request method and URL
+  xhr.open("POST", "contact.php");
+
+  // Set the request header
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  // Set the response type
+  xhr.responseType = "json";
+
+  // Set the onload event handler
+  xhr.onload = function () {
+    // Get the response data
+    const response = xhr.response;
+
+    // Display the response message in the result div
+    document.querySelector("#result").innerHTML = response.message;
+  };
+
+  // Set the onerror event handler
+  xhr.onerror = function () {
+    // Display an error message in the result div
+    document.querySelector("#result").innerHTML =
+      "An error occurred while sending the form data.";
+  };
+
+  // Send the form data
+  xhr.send(
+    `name=${name}&tel=${tel}&email=${email}&wherefrom=${wherefrom}&whereto=${whereto}&message=${message}`
+  );
 });
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
